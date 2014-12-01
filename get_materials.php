f451934f092e05f99a09822185a903cb45aae3a8
Yukich <navighnsit@gmail.com>
2014-12-01 20:05:11 +0900

    mod get_materials.php

--- get_materials.php
+++ get_materials.php
@@ -10,7 +10,7 @@ if(!$conn = mysql_connect($host, "s1413137", "s1413137hoge")){
 }
 mysql_select_db("s1413137", $conn);
 mysql_set_charset("utf8");
-$sql = "select * from materialss";
+$sql = "select * from materials";
 $res = mysql_query($sql, $conn);
 while($row = mysql_fetch_array($res)){
         print("<tr>");

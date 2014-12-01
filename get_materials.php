c16a29e8e2e0718337eef855dd79d3dca0540a36
Yukich <navighnsit@gmail.com>
2014-12-01 19:54:09 +0900

    7phpfiles add

---
+++ get_materials.php
@@ -0,0 +1,24 @@
+<html>
+<head><title>原材料テーブル</title></head> 
+<body>
+<table border="1">
+<tr><th>原材料名</tr>
+<?php
+$host = "localhost";
+if(!$conn = mysql_connect($host, "s1413137", "s1413137hoge")){
+        die("MySQL接続エラー.<br />");
+}
+mysql_select_db("s1413137", $conn);
+mysql_set_charset("utf8");
+$sql = "select * from materialss";
+$res = mysql_query($sql, $conn);
+while($row = mysql_fetch_array($res)){
+        print("<tr>");
+        print("<td>".$row["name"]."</td>");
+        print("</tr>\n");
+}
+mysql_free_result($res);
+?>
+</table>
+</body>
+</html>
---
+++ get_uses.php
@@ -0,0 +1,25 @@
+<html>
+<head><title>使用テーブル</title></head> 
+<body>
+<table border="1">
+<tr><th>商品ID</th><th>原材料名</th></tr>
+<?php
+$host = "localhost";
+if(!$conn = mysql_connect($host, "s1413137", "s1413137hoge")){
+        die("MySQL接続エラー.<br />");
+}
+mysql_select_db("s1413137", $conn);
+mysql_set_charset("utf8");
+$sql = "select * from uses";
+$res = mysql_query($sql, $conn);
+while($row = mysql_fetch_array($res)){
+        print("<tr>");
+        print("<td>".$row["item_id"]."</td>");
+        print("<td>".$row["material_name"]."</td>");
+        print("</tr>\n");
+}
+mysql_free_result($res);
+?>
+</table>
+</body>
+</html>

4717b28643b2aeb972f3e28803dc8e273440ce21
Yukich <navighnsit@gmail.com>
2014-12-01 19:18:40 +0900

    bugfix

--- meshiya_create.sql
+++ meshiya_create.sql
@@ -1,39 +1,39 @@
 create table meshiyas(
-	name varchar(200) primary key,
-	address varchar(200),
-	category varchar(200),
-	hours varchar(200),
-	holiday varchar(200)
+	name varchar(20) primary key,
+	address text,
+	category varchar(20),
+	hours text,
+	holiday text
 );
 create table items(
 	id int primary key auto_increment,
-	name varchar(200),
+	name varchar(20),
 	picture text,
 	remarks text
 );
 create table users(
 	id int primary key auto_increment,
 	name varchar(20),
-	password varchar(16)
+	password varchar(20)
 );
 create table offers(
-	meshiya_name varchar(200),
+	meshiya_name varchar(20),
 	item_id int,
 	price int unsigned,
 	primary key(meshiya_name, item_id)
 );
 create table reviews(
-	meshiya_name varchar(200),
+	meshiya_name varchar(20),
 	user_id int,
 	value int,
 	comment text,
 	primary key(meshiya_name, user_id)
 );
 create table materials(
-	name varchar(200) primary key
+	name varchar(20) primary key
 );
 create table uses(
-	item_name varchar(200),
-	material_name varchar(200),
+	item_name varchar(20),
+	material_name varchar(20),
 	primary key(item_name, material_name)
 );

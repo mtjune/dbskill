b6c20077f734a457ad48d76141f5905469ee54f8
Yukich <navighnsit@gmail.com>
2014-12-01 18:57:32 +0900

    2 file add

--- meshiya_create.sql
+++ meshiya_create.sql
@@ -1,40 +1,39 @@
 create table meshiyas(
-	name varchar(200) primary key,	// 名前
-	address varchar(200),					// 住所
-	category varchar(200),				// カテゴリ
-	hours varchar(200),					// 営業時間
-	holiday varchar(200)					// 定休日
+	name varchar(200) primary key,
+	address varchar(200),
+	category varchar(200),
+	hours varchar(200),
+	holiday varchar(200)
 );
 create table items(
-	name varchar(200) primary key,	// 名前
-	materials text,							// 原材料
-	allergy text,								// アレルギー表示
-	picture text,								// 写真
-	meshiya_name varchar(200),		// 飯屋の名前
+	id int primary key auto_increment,
+	name varchar(200),
+	picture text,
+	remarks text
 );
 create table users(
-	id int primary key auto_increment,	// ユーザID
-	name varchar(20),							// ユーザ名
-	password varchar(16)						// パスワード
+	id int primary key auto_increment,
+	name varchar(20),
+	password varchar(16)
 );
 create table offers(
-	meshiya_name varchar(200),	// 飯屋名
-	item_name varchar(200),			// 商品名
-	price int unsigned					// 価格
+	meshiya_name varchar(200),
+	item_id int,
+	price int unsigned,
 	primary key(meshiya_name, item_name)
 );
 create table reviews(
-	meshiya_name varchar(200),	// 飯屋の名前
-	user_id int,							// ユーザID
-	value int,								// 評価値
-	comment text,						// コメント
+	meshiya_name varchar(200),
+	user_id int,
+	value int,
+	comment text,
 	primary key(meshiya_name, user_id)
 );
 create table materials(
-	name varchar(200) primary key		// 原材料名
+	name varchar(200) primary key
 );
 create table uses(
-	item_name varchar(200),			// 商品名
-	material_name varchar(200),	// 原材料名
+	item_name varchar(200),
+	material_name varchar(200),
 	primary key(item_name, material_name)
 );
---
+++ meshiya_insert.sql
@@ -0,0 +1,18 @@
+insert into meshiyas values("龍郎", "吾妻", "ラーメン", "11：30～14：30, 17：30～22：00（材料切れ終了）", "月曜日");
+
+insert into items(name, picture, remarks) values("ラーメン", "pic1(保留)", "備考");
+insert into items(name, picture, remarks) values("まぜそば", "pic2(保留)", "備考");
+
+insert into users(name, password) values("やまじゅん", "yamayama");
+
+insert into offers values("龍郎", 0, 680);
+insert into offers values("龍郎", 1, 750);
+
+insert into reviews values("龍郎", 0, 5, "うまい！");
+
+insert into materials values("小麦粉");
+insert into materials values("豚肉");
+
+insert into uses values(0, "小麦粉");
+insert into uses values(0, "豚肉");
+

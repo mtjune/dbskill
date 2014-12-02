569e07e84ac188f1863e33d4e6b36b5d064a4dea
Yukich <navighnsit@gmail.com>
2014-12-02 17:34:11 +0900

    add getphps

---
+++ getphps/get_materials.php
@@ -0,0 +1,25 @@
+<html>
+<head><title>原材料テーブル</title>
+<link rel="stylesheet" type="text/css" href="style.css"></head> 
+<body>
+<table border="1">
+<tr><th>原材料名</tr>
+<?php
+$host = "localhost";
+if(!$conn = mysql_connect($host, "s1413137", "s1413137hoge")){
+        die("MySQL接続エラー.<br />");
+}
+mysql_select_db("s1413137", $conn);
+mysql_set_charset("utf8");
+$sql = "select * from materials";
+$res = mysql_query($sql, $conn);
+while($row = mysql_fetch_array($res)){
+        print("<tr>");
+        print("<td>".$row["name"]."</td>");
+        print("</tr>\n");
+}
+mysql_free_result($res);
+?>
+</table>
+</body>
+</html>
---
+++ getphps/get_reviews.php
@@ -0,0 +1,28 @@
+<html>
+<head><title>レビューテーブル</title>
+<link rel="stylesheet" type="text/css" href="style.css"></head> 
+<body>
+<table border="1">
+<tr><th>飯屋名</th><th>ユーザID</th><th>評価値</th><th>コメント</th></tr>
+<?php
+$host = "localhost";
+if(!$conn = mysql_connect($host, "s1413137", "s1413137hoge")){
+        die("MySQL接続エラー.<br />");
+}
+mysql_select_db("s1413137", $conn);
+mysql_set_charset("utf8");
+$sql = "select * from reviews";
+$res = mysql_query($sql, $conn);
+while($row = mysql_fetch_array($res)){
+        print("<tr>");
+        print("<td>".$row["meshiya_name"]."</td>");
+        print("<td>".$row["user_id"]."</td>");
+        print("<td>".$row["value"]."</td>");
+        print("<td>".$row["comment"]."</td>");
+        print("</tr>\n");
+}
+mysql_free_result($res);
+?>
+</table>
+</body>
+</html>

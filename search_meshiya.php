29b7bad0af6ac59ce096b705e44f217244c63d8f
Yukich <navighnsit@gmail.com>
2014-12-03 09:24:06 +0900

    add address search

--- search_meshiya.php
+++ search_meshiya.php
@@ -13,18 +13,33 @@ if(!$conn = mysql_connect($host, "s1413137", "s1413137hoge")){
 mysql_select_db("s1413137", $conn);
 mysql_set_charset("utf8");
 
-$meshiya_name = $_POST['meshiya_name'];
+$meshiya_name = $_POST['name'];
+$address = $_POST['address'];
 
-$sql = "select * from meshiyas where meshiyas.name like \"%".$meshiya_name."%\";";
+$sql = "select * from meshiyas where (meshiyas.name like \"%".$meshiya_name."%\")";
+
+if ($address[0] != "no_address" || sizeof($address) == 0) {
+	$sql = $sql." and (";
+	
+	for ($i=0; $i < sizeof($address); $i++) { 
+		$sql = $sql."meshiyas.address like \"%".$address."\"";
+		if($i + 1 < sizeof($address)){
+			$sql = $sql." or";
+		} else {
+			$sql = $sql.";";
+		}
+	}
+}
+
 $res = mysql_query($sql, $conn);
 while($row = mysql_fetch_array($res)){
-        print("<tr>");
-        print("<td>".$row["name"]."</td>");
-        print("<td>".$row["address"]."</td>");
-        print("<td>".$row["category"]."</td>");
-        print("<td>".$row["hours"]."</td>");
-        print("<td>".$row["holiday"]."</td>");
-        print("</tr>\n");
+    print("<tr>");
+    print("<td>".$row["name"]."</td>");
+    print("<td>".$row["address"]."</td>");
+    print("<td>".$row["category"]."</td>");
+    print("<td>".$row["hours"]."</td>");
+    print("<td>".$row["holiday"]."</td>");
+    print("</tr>\n");
 }
 mysql_free_result($res);
 ?>

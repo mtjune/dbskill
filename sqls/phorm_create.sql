195bd41434f27e94b325976c6588d20084e4b1e5
Yukich <navighnsit@gmail.com>
2014-12-08 23:08:18 +0900

    bugfix

--- sqls/phorm_create.sql
+++ sqls/phorm_create.sql
@@ -5,17 +5,17 @@ create table pics(
 	remarks text,
 	user_id int,
 	post_date date
-)
+);
 
 create table users(
 	id int primary key auto_increment,
 	name varchar(20),
 	pass varchar(20)
-)
+);
 
 create table tags(
 	name varchar(20) primary key
-)
+);
 
 create table evaluations(
 	pic_id int,
@@ -23,16 +23,16 @@ create table evaluations(
 	value int,
 	primary key(pic_id, user_id),
 	check(value >= 1 and value <= 5)
-)
+);
 
 create table additions(
 	tag_name varchar(20),
 	pic_id int,
 	primary key(tag_name, pic_id)
-)
+);
 
 create table bookmarks(
 	user_id int,
 	tag_name varchar(20),
 	primary key(user_id, tag_name)
-)
+);

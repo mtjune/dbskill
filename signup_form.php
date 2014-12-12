4e3144a258f6d545a43199a3173d79e46aba5654
Yukich <navighnsit@gmail.com>
2014-12-13 00:19:12 +0900

    shintyoku

--- signup_form.php
+++ signup_form.php
@@ -8,15 +8,6 @@ if(!$conn = mysql_connect($host, "s1413137", "s1413137hoge")){
 }
 mysql_select_db("s1413137", $conn);
 mysql_set_charset("utf8");
-
-if($is_login){
-    $login_user_id = $_SESSION['user_id'];
-    mysql_query("select name from users where id = '$login_user_id'");
-    $res = mysql_query($sql, $conn);
-    $row = mysql_fetch_assoc($res);
-    $login_user_name = $row['name'];
-    mysql_free_result($res);
-}
 ?>
 
 <!DOCTYPE html>
@@ -24,7 +15,7 @@ if($is_login){
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, user-scalable=yes, maximum-scale=1.0, minimum-scale=1.0">
-<title>Phorm : 写真投稿サイト</title>
+<title>ユーザ登録 - Phorm</title>
 <meta name="keywords" content="">
 <meta name="description" content="">
 <link rel="stylesheet" href="style.css" type="text/css" media="screen">
@@ -36,7 +27,7 @@ if($is_login){
 <script src="js/script.js"></script>
 </head>
 
-<body>
+<body id="subpage">
 <header id="header">
   <h1>Photos Posted Site.</h1>
   
@@ -53,6 +44,7 @@ if(!$is_login){
     print("<table><tr><td>ユーザID</td><td><input type='text' name='login_user_id'></td></tr><tr><td>パスワード</td><td><input type='password' name='login_user_pass'></td></tr><tr><td colspan='2'><input type='submit' value='ログイン'><td></tr></table>");
     print("</form>");
 } else {
+    $login_user_id = $_SESSION['user_id'];
     $sql = "select name from users where id = '$login_user_id'";
     $res = mysql_query($sql, $conn);
     $row = mysql_fetch_assoc($res);
@@ -74,73 +66,49 @@ if(!$is_login){
         <div class="panel">   
         <ul>
             <li class="active"><a href="index.php"><strong>トップページ</strong><span>Top</span></a></li>
-                <li><a href="bookmark_new.php"><strong>ブックマーク新着</strong><span>Bookmark</span></a></li>
+                <li><a href="subpage.html"><strong>ブックマーク新着</strong><span>Bookmark</span></a></li>
                 <li><a href="userpage.php"><strong>ユーザーページ</strong><span>User Page</span></a></li>
-                <li><a href="picpost_form.php"><strong>写真投稿</strong><span>Photo Post</span></a></li>
-                <li><a href="signup_form.php"><strong>ユーザ登録</strong><span>Sign Up</span></a></li>
+                <li><a href="picpost_form.html"><strong>写真投稿</strong><span>Photo Post</span></a></li>
+                <li><a href="signup_form.html"><strong>ユーザ登録</strong><span>Sign Up</span></a></li>
                 <li class="last"><a href="get_tables.php"><strong>全テーブルを表示</strong><span>Show Tables</span></a></li>
             </ul>   
     </div>
     </div> 
 </nav>
 <!-- / メインナビゲーション -->
-  
+
 <div id="wrapper">
   
-  <!-- メイン画像 -->
-    <div id="mainBanner">
-        <img src="images/Phorm_top_logo.png" width="50%" alt="Phorm" class="alignleft border">
-        <p>
-        <br>
-        <form action="search.php" method="get">
-        <table>
-            <tr><td>写真を探す</td></tr>
-            <tr><td><input type="text" name="word"></td><td><input type="submit" value="検索"></td></tr>
-        </table>
-        <table>
-            <tr>
-                <td><input type="radio" name="search_mode" value="keyword" checked></td>
-                <td>キーワード検索</td>
-            </tr>
-            <tr>
-                <td><input type="radio" name="search_mode" value="title"></td>
-                <td>タイトル検索</td>
-            </tr>
-            <tr>
-                <td><input type="radio" name="search_mode" value="tag"></td>
-                <td>タグ検索</td>
-            </tr>
-        </table>
-        </form>
-        </p>
-    </div>
-    <!-- / メイン画像 -->
-    
+  <!-- コンテンツ -->
+        <section id="main">
+<section class="content">
+<h3 class="heading">ユーザ登録フォーム</h3>
+<article>
+<form action="signup.php" method="post">
+<table>
+<tr>
+	<td>ユーザID</td><td><input type="text" name="user_id" maxlength="16"></td>
+</tr>
+<tr>
+	<td>パスワード</td><td><input type="password" name="user_pass" maxlength="16"></td>
+</tr>
+<tr>
+	<td>ユーザ名</td><td><input type="text" name="user_name" maxlength="16"></td>
+</tr>
+</table>
+<input type="submit" value="登録" />
+</form>
+</article>
+</section>
+    </section>
+        <!-- / コンテンツ -->
 
-  
-</div>
- <!-- フッター -->
-<footer id="footer">
-    <div class="inner">
-    <!-- 左側 -->
-        <div id="info" class="grid">
-            <!-- ロゴ -->
-            
-            <!-- / ロゴ -->
-            <!-- 電話番号+受付時間 -->
-            <div class="info">
-                <p class="tel"><span>作成者:</span> 山田純也</p>
-                <p class="open">学籍番号: 201413137</p>
-            </div>
-            <!-- / 電話番号+受付時間 -->
-        </div>  
-        <!-- / 左側 -->
-        <!-- 右側 ナビゲーション -->
+        <aside id="sidebar">
         
-        <!-- / 右側 ナビゲーション -->
-    </div>
-</footer>
-<!-- / フッター -->
+        </aside>
+ 
+</div>
+ 
 <address>Copyright(c) 2013 Sample Inc. All Rights Reserved. Design by <a href="http://f-tpl.com" target="_blank" rel="nofollow">http://f-tpl.com</a></address>
 
 </body>

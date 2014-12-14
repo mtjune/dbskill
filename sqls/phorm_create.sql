ccd4009d60ae7695603c6e660ed7aad63fc8f25f
Yukich <navighnsit@gmail.com>
2014-12-14 13:19:58 +0900

    shintyoku

--- sqls/phorm_create.sql
+++ sqls/phorm_create.sql
@@ -3,14 +3,14 @@ create table pics(
 	title varchar(20),
 	file_name text,
 	remarks text,
-	user_id varchar(20),
+	user_id varchar(20) references users(id),
 	post_date date
 );
 
 create table users(
 	id varchar(20) primary key,
 	name varchar(20),
-	pass varchar(20)
+	pass varchar(100)
 );
 
 create table tags(
@@ -18,21 +18,21 @@ create table tags(
 );
 
 create table evaluations(
-	pic_id int,
-	user_id varchar(20),
+	pic_id int references pics(id),
+	user_id varchar(20) references users(id),
 	value int,
 	primary key(pic_id, user_id),
 	check(value >= 1 and value <= 5)
 );
 
 create table additions(
-	tag_name varchar(20),
-	pic_id int,
+	tag_name varchar(20) references tags(name),
+	pic_id int references pics(id),
 	primary key(tag_name, pic_id)
 );
 
 create table bookmarks(
-	user_id varchar(20),
-	tag_name varchar(20),
+	user_id varchar(20) references users(id),
+	tag_name varchar(20) references tans(name),
 	primary key(user_id, tag_name)
 );
